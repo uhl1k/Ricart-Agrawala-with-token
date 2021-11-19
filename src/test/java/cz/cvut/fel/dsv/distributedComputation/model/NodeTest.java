7d619ca1b3b2bf6a439964802755795f0ede938d
Roman Janků <jankurom@fel.cvut.cz>
2021-11-19 14:00:03 +0100

    basic tests for Node, more will come later as the implementation grows

--- src/test/java/cz/cvut/fel/dsv/distributedComputation/model/NodeTest.java
+++ src/test/java/cz/cvut/fel/dsv/distributedComputation/model/NodeTest.java
@@ -1,3 +1,44 @@
 package cz.cvut.fel.dsv.distributedComputation.model;
 
-class NodeTest {}
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class NodeTest {
+  @Test
+  void constructor_nodeFromNodeIDAndAddressAndPortShouldBeSame() {
+    Node node1 = null;
+    Node node2 = null;
+    try {
+      node1 = Node.getNodeFromNodeID(new NodeID(InetAddress.getByName("10.0.0.1"), 123));
+      node2 = Node.getNodeFromAddressAndPort(InetAddress.getByName("10.0.0.1"), 123);
+    } catch (UnknownHostException ex) {
+      fail();
+    }
+    assertEquals(node1, node2);
+  }
+
+  @Test
+  void constructor_nodeFromNodeIDAndAddressAndPortShouldNotBeSameIfDifferentIPOrPort() {
+    Node node1 = null;
+    Node node2 = null;
+    Node node3 = null;
+    try {
+      node1 = Node.getNodeFromNodeID(new NodeID(InetAddress.getByName("10.0.0.1"), 123));
+      node2 = Node.getNodeFromAddressAndPort(InetAddress.getByName("192.168.1.200"), 123);
+      node3 = Node.getNodeFromNodeID(new NodeID(InetAddress.getByName("10.0.0.1"), 125));
+    } catch (UnknownHostException ex) {
+      fail();
+    }
+    assertNotEquals(node1, node2);
+    assertNotEquals(node1, node3);
+    assertNotEquals(node2, node3);
+  }
+
+  //  compareTo(), hashCode() and equals() are not tested as they rely on NodeId methods which have
+  //  been thoroughly tested
+}
